c5728fad849b3b10b77ef1b7f09a8323eb3773a1
Giovanni Tramontano <giovanni.tramontano.gt@gmail.com>
2025-03-05 18:53:34 +0100

    Aggiunta favicon personalizzata

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title}}</title>
+    <link rel="icon" type="image/x-icon" href="/myFavIcon.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
